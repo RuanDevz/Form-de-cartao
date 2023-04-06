efa1c25c59d6060ed6af2f4740ff2227f20ee908
Ruan Pedro Batista da Silva <121466178+RuanDevz@users.noreply.github.com>
2023-04-06 10:40:26 -0300

    ok

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
-const numerocartao = document.getElementById('numero')
-const nometitular = document.getElementById('nome')
-const validade = document.getElementById('validade')
-const cvv = document.getElementById('cvv')
-const info = document.getElementById('info')
-  
-numerocartao.addEventListener("input", (event) => {
-    const input = event.target;
-    const unformattedValue = input.value.replace(/\s+/g, "");
-    const formattedValue = unformattedValue.replace(/(\d{1,4})/g, "$1 ").trim();
-  
-    input.value = formattedValue;
-  });
+const cartao = document.getElementById('numero');
+const validade = document.getElementById('validade');
+const cvv = document.getElementById('cvv');
+const anoMaximo = new Date().getFullYear() + 10;
+
+IMask(cartao,{
+    mask: '0000 0000 0000 0000'
+});
+
+IMask(validade,{
+    mask: '00/00',
+    blocks: {
+        1: {
+            mask: IMask.MaskedRange,
+            from: 1,
+            to: 12,
+        },
+        2: {
+            mask: IMask.MaskedRange,
+            from: new Date().getFullYear().toString().substring(2),
+            to: anoMaximo.toString().substring(2),
+        }
+    },
+});
+
+IMask(cvv,{
+    mask: '0000'
+});
